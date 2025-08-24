dc970deaa77617ab5d1729c582a33c189e23028e
ВьLachezar Uzunov <lachezar.z.uzunov@gmail.com>
2025-08-24 15:18:52 +0300

    add: add initial files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { Button } from "@/components/ui/button";
+
+function App() {
+
+
+  return (
+    <>
+        <div>
+            <h1 className='text-6xl underline'>Hello world</h1>
+            <div className="flex min-h-svh flex-col items-center justify-center">
+                <Button>Click me</Button>
+            </div>
+        </div>
+
+    </>
+  )
+}
+
+export default App
